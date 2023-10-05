37972d9fd72c05049c673d84fde9bfae5f90b900
oleksandr leoshko <sleo170504@gmail.com>
2023-10-05 20:19:47 +0300

    Divide into small functions Add input for username and password Maybe something else...

--- main.py
+++ main.py
@@ -5,10 +5,74 @@ import time
 from webdriver_manager.chrome import ChromeDriverManager
 import config as cf
 from selenium.webdriver.common.by import By
-from config import username_cf, password_cf
 
 
 def start():
+    username, password = enter_log_pass()
+
+    driver = run_driver()
+
+    driver.get(
+        "https://accounts.google.com/ServiceLogin/identifier?service=classroom&passive=1209600&continue=https%3A%2F%2Fclassroom.google.com%2F&followup=https%3A%2F%2Fclassroom.google.com%2F&emr=1&flowName=GlifWebSignIn&flowEntry=AddSession")
+
+    # Enter username
+    enter_text(driver, By.ID, 'identifierId', username)
+    click_element(driver, By.XPATH, '//*[@id="identifierNext"]/div/button/span')
+    time.sleep(5)
+
+    # Enter password
+    enter_text(driver, By.XPATH, '//*[@id="password"]/div[1]/div/div[1]/input', password)
+    click_element(driver, By.XPATH, '//*[@id="passwordNext"]/div/button/span')
+    time.sleep(5)
+
+    # Find and enter the needed class
+
+    # For now, it manually added class name that we need to click
+    target_class = "2023 (осінь) Завадостійке кодування та захист біомедичної інфор"
+
+    elements = driver.find_elements(By.CLASS_NAME, "YVvGBb.z3vRcc-ZoZQ1")
+
+    # Parser
+    # element_text_list = [element.text for element in elements]
+
+    for element in elements:
+        if element.text == target_class:
+            element.click()
+            break
+
+    # Click on button to join Meet conference
+    time.sleep(2)
+    meet_button = find_element(driver, By.XPATH,
+                               '//*[@id="yDmH0d"]/c-wiz[2]/div[2]/div/div[7]/div[2]/aside/div/div[1]/div/div[2]/div/a')
+    meet_button.click()
+
+    # Switch to tab with conference
+    driver.switch_to.window(driver.window_handles[1])
+    time.sleep(2)
+
+    off_mic_video(driver)
+
+    # Click on Join button
+    join_button = find_element(driver, By.XPATH,
+                                      '//*[@id="yDmH0d"]/c-wiz/div/div/div[15]/div[3]/div/div[2]/div[4]/div/div/div['
+                                      '2]/div[1]/div[2]/div[1]/div[1]')
+    join_button.click()
+
+    time.sleep(5000)
+
+
+def off_mic_video(driver):
+    elements = find_elements(driver, By.CLASS_NAME, 'GKGgdd')
+    print(elements)
+
+    elements[0].click()
+    time.sleep(0.5)
+
+    elements[1].click()
+    time.sleep(0.5)
+
+
+def run_driver():
     options = webdriver.ChromeOptions()
     options.binary_location = r"C:/Program Files/Google/Chrome/Application/chrome.exe"
     options.add_argument("start-maximized")
@@ -26,80 +90,32 @@ def start():
     service = Service(service_path)
     driver = webdriver.Chrome(service=service, options=options)
 
-    driver.get(
-        "https://accounts.google.com/ServiceLogin/identifier?service=classroom&passive=1209600&continue=https%3A%2F%2Fclassroom.google.com%2F&followup=https%3A%2F%2Fclassroom.google.com%2F&emr=1&flowName=GlifWebSignIn&flowEntry=AddSession")
+    return driver
 
-    # Enter username
-    username = driver.find_element(By.ID, 'identifierId')
-    username.click()
-    username.send_keys(username_cf)
-    proceed = driver.find_element(By.XPATH, '//*[@id="identifierNext"]/div/button/span')
-    proceed.click()
-    time.sleep(5)
 
-    # Enter password
-    password = driver.find_element(By.XPATH, '//*[@id="password"]/div[1]/div/div[1]/input')
-    password.click()
-    password.send_keys(password_cf)
-    proceed2 = driver.find_element(By.XPATH, '//*[@id="passwordNext"]/div/button/span')
-    proceed2.click()
+def find_element(driver, by, value):
+    return driver.find_element(by, value)
 
-    # Find and enter the needed class
-    time.sleep(6)
 
-    # For now, it manually added class name that we need to click
-    target_class = "2023 (осінь) Завадостійке кодування та захист біомедичної інфор"
+def find_elements(driver, by, value):
+    return driver.find_elements(by, value)
 
-    elements = driver.find_elements(By.CLASS_NAME, "YVvGBb.z3vRcc-ZoZQ1")
 
-    print(elements)
+def click_element(driver, by, value):
+    element = find_element(driver, by, value)
+    element.click()
 
-    element_text_list = []
 
-    # Small parser of all classes name
-    for element in elements:
-        element_text_list.append(element.text)
+def enter_text(driver, by, value, text):
+    element = find_element(driver, by, value)
+    element.click()
+    element.send_keys(text)
 
-    for element in elements:
-        if element.text == target_class:
-            element.click()
-            break
 
-    print(element_text_list)
-    print(type(element_text_list))
-
-    # Click on button to join Meet conference
-    time.sleep(2)
-    meet_button = driver.find_element(By.XPATH,
-                                      '//*[@id="yDmH0d"]/c-wiz[2]/div[2]/div/div[7]/div[2]/aside/div/div[1]/div/div[2]/div/a')
-    meet_button.click()
-
-    # Switch to tab with conference
-    driver.switch_to.window(driver.window_handles[1])
-    time.sleep(2)
-
-    # Find all elements with the class "GKGgdd"
-    elements = driver.find_elements(By.CLASS_NAME, 'GKGgdd')
-
-    # Click on the first element
-    if elements:
-        elements[0].click()
-    time.sleep(0.5)
-
-    # Find all elements with the class "GKGgdd" again (to get the second element)
-    elements = driver.find_elements(By.CLASS_NAME, 'GKGgdd')
-
-    # Click on the second element
-    if len(elements) >= 2:
-        elements[1].click()
-    time.sleep(0.5)
-
-    # Click on Join button
-    join_button = driver.find_element(By.XPATH,
-                                      '//*[@id="yDmH0d"]/c-wiz/div/div/div[15]/div[3]/div/div[2]/div[4]/div/div/div[2]/div[1]/div[2]/div[1]/div[1]')
-    join_button.click()
-
-    time.sleep(5000)
+def enter_log_pass():
+    username = input("Enter your school Gmail: ")
+    password = input("Enter your password: ")
+    return username, password
 
 
 def main():
