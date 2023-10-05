8d84d56c8e79f425cacf1849f35e2eee3519771e
oleksandr leoshko <sleo170504@gmail.com>
2023-10-05 20:20:06 +0300

    Divide into small functions Add input for username and password Maybe something else...

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
+
 AMOUNT_THREAD = 1
 
 local = {
     "executablePath": r"C:\\Users\\sleo1\\PycharmProjects\\automeet\\chromedriver.exe",
     "userDataDir": r"C:\\Users\\sleo1\\AppData\\Local\\Google\\Chrome\\User Data",
 }
-
-username_cf = ""
-password_cf = ""
